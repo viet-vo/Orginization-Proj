59772f741d540735440264d0f33f672b2cd031a4
Viet Vo <viet.allen.qvo@gmail.com>
2020-01-02 23:26:09 -0800

    merged section of working MainPage onto Head

--- client/index.js
+++ client/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MainPage from './components/pages/MainPage';
 
-import MainPage from './components/pages/MainPage';
-
 class App extends React.Component{
     render(){
         return(
